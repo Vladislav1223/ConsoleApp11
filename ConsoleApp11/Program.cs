ea286f073e28a3bfcfe3d9ad56a271c1cc2d6f46
Vladislav <vlad.king.20012109@gmail.com>
2021-04-07 22:25:32 +0300

    11

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp11
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
